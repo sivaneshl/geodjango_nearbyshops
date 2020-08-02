6f2ea8fd91fb375245c42a4944d6644e7fca3cf3
Sivanesh Logandurai <sivanesh.l@gmail.com>
2020-08-02 15:04:29 +0530

    project and db setup

--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.PointField()
+    address = models.CharField(max_length=100)
